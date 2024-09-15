acd5339f36ae8d58fcfc62b9d6e86bf2dccd080e
Dianna Brown <DiBrown@redhat.com>
2024-09-15 02:08:49 -0500

    Create Main.java

---
+++ src/java/myprogram/Main.java
@@ -0,0 +1,11 @@
+
+package myprogram;
+
+import myprogram.utils.*;
+
+public final class Main {
+    
+    public static void main(String[] args) {
+        System.out.println(Util.get(1337));
+    }
+}
